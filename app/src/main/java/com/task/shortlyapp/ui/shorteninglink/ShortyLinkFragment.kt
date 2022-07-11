e8f0f39638a25bacbd32692c4fe885d54cff1968
Muhammad Maqsood <muhammad.maqsood@avrioc.com>
2022-07-11 10:06:37 +0500

    Fix: Unused import removed

--- app/src/main/java/com/task/shortlyapp/ui/shorteninglink/ShortyLinkFragment.kt
+++ app/src/main/java/com/task/shortlyapp/ui/shorteninglink/ShortyLinkFragment.kt
@@ -4,7 +4,6 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -84,7 +83,6 @@ class ShortyLinkFragment : Fragment(), ShortyLinkView, View.OnClickListener {
                     onShortlyError(state.error as? String)
                 }
                 is NetworkState.Loading -> {
-
                 }
             }
         }
